3fe51e6b6585f9e6dd9d9416099403e899957044
Tuttee <111608871+Tuttee@users.noreply.github.com>
2025-03-25 10:42:16 +0200

    - Int tests

--- src/main/java/com/facts_svc/repository/FactRepository.java
+++ src/main/java/com/facts_svc/repository/FactRepository.java
@@ -4,8 +4,10 @@ import com.facts_svc.model.Fact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FactRepository extends JpaRepository<Fact, UUID> {
+    Optional<Fact> findByContent(String content);
 }
